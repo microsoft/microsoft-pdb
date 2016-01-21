2252a22822cff01394f23db083866c54ea10e2d9
Jim Radigan <jradigan@microsoft.com>
2016-01-20 21:43:18 -0800

    Create cvtdef.h

---
+++ cvdump/cvtdef.h
@@ -0,0 +1,132 @@
+/***********************************************************************
+* Microsoft (R) Debugging Information Dumper
+*
+* Copyright (c) Microsoft Corporation.  All rights reserved.
+*
+* File Comments:
+*
+*
+***********************************************************************/
+
+// these typedefs define the pointers to the cvinfo.h structures
+
+typedef TYPTYPE         *TYPPTR;
+typedef lfEasy          *plfEasy;
+typedef lfModifier      *plfModifier;
+typedef lfPointer       *plfPointer;
+typedef lfArray         *plfArray;
+typedef lfClass         *plfClass;
+typedef lfStructure     *plfStructure;
+typedef lfUnion         *plfUnion;
+typedef lfEnum          *plfEnum;
+typedef lfProc          *plfProc;
+typedef lfMFunc         *plfMFunc;
+typedef lfVTShape       *plfVTShape;
+typedef lfVftable       *plfVftable;
+typedef lfCobol0        *plfCobol0;
+typedef lfCobol1        *plfCobol1;
+typedef lfBArray        *plfBArray;
+typedef lfLabel         *plfLabel;
+typedef lfSkip          *plfSkip;
+typedef lfArgList       *plfArgList;
+typedef lfDerived       *plfDerived;
+typedef lfPreComp       *plfPreComp;
+typedef lfAlias         *plfAlias;
+typedef lfEndPreComp    *plfEndPreComp;
+typedef lfDefArg        *plfDefArg;
+typedef lfList          *plfList;
+typedef lfFieldList     *plfFieldList;
+typedef mlMethod        *pmlMethod;
+typedef lfMethodList    *plfMethodList;
+typedef lfBitfield      *plfBitfield;
+typedef lfChar          *plfChar;
+typedef lfShort         *plfShort;
+typedef lfUShort        *plfUShort;
+typedef lfLong          *plfLong;
+typedef lfULong         *plfULong;
+typedef lfReal32        *plfReal32;
+typedef lfReal64        *plfReal64;
+typedef lfReal80        *plfReal80;
+typedef lfReal128       *plfReal128;
+typedef lfIndex         *plfIndex;
+typedef lfBClass        *plfBClass;
+typedef lfVBClass       *plfVBClass;
+typedef lfFriendCls     *plfFriendCls;
+typedef lfFriendFcn     *plfFriendFcn;
+typedef lfMember        *plfMember;
+typedef lfSTMember      *plfSTMember;
+typedef lfVFuncTab      *plfVFuncTab;
+typedef lfMethod        *plfMethod;
+typedef lfOneMethod     *plfOneMethod;
+typedef lfEnumerate     *plfEnumerate;
+typedef lfNestType      *plfNestType;
+typedef lfTypeServer    *plfTypeServer;
+typedef SYMTYPE         *SYMPTR;
+typedef CFLAGSYM        *CFLAGPTR;
+typedef CONSTSYM        *CONSTPTR;
+typedef OBJNAMESYM      *OBJNAMEPTR;
+typedef REGSYM          *REGPTR;
+typedef UDTSYM          *UDTPTR;
+typedef SEARCHSYM       *SEARCHPTR;
+typedef BLOCKSYM16      *BLOCKPTR16;
+typedef DATASYM16       *DATAPTR16;
+typedef PUBSYM16        *PUBPTR16;
+typedef LABELSYM16      *LABELPTR16;
+typedef BPRELSYM16      *BPRELPTR16;
+typedef PROCSYM16       *PROCPTR16;
+typedef THUNKSYM16      *THUNKPTR16;
+typedef CEXMSYM16       *CEXMPTR16;
+typedef WITHSYM16       *WITHPTR16;
+typedef BLOCKSYM32      *BLOCKPTR32;
+typedef DATASYM32       *DATAPTR32;
+typedef PUBSYM32        *PUBPTR32;
+typedef LABELSYM32      *LABELPTR32;
+typedef BPRELSYM32      *BPRELPTR32;
+typedef PROCSYM32       *PROCPTR32;
+typedef THUNKSYM32      *THUNKPTR32;
+typedef WITHSYM32       *WITHPTR32;
+typedef BLOCKSYM        *BLOCKPTR;
+typedef PROCSYM         *PROCPTR;
+typedef THUNKSYM        *THUNKPTR;
+typedef WITHSYM         *WITHPTR;
+
+typedef lfModifier_16t  *plfModifier_16t;
+typedef lfPointer_16t   *plfPointer_16t;
+typedef lfArray_16t     *plfArray_16t;
+typedef lfClass_16t     *plfClass_16t;
+typedef lfStructure_16t *plfStructure_16t;
+typedef lfUnion_16t     *plfUnion_16t;
+typedef lfEnum_16t      *plfEnum_16t;
+typedef lfProc_16t      *plfProc_16t;
+typedef lfMFunc_16t     *plfMFunc_16t;
+typedef lfCobol0_16t    *plfCobol0_16t;
+typedef lfBArray_16t    *plfBArray_16t;
+typedef lfSkip_16t      *plfSkip_16t;
+typedef lfArgList_16t   *plfArgList_16t;
+typedef lfDerived_16t   *plfDerived_16t;
+typedef lfPreComp_16t   *plfPreComp_16t;
+typedef lfDefArg_16t    *plfDefArg_16t;
+typedef lfFieldList_16t *plfFieldList_16t;
+typedef mlMethod_16t    *pmlMethod_16t;
+typedef lfMethodList_16t*plfMethodList_16t;
+typedef lfBitfield_16t  *plfBitfield_16t;
+typedef lfIndex_16t     *plfIndex_16t;
+typedef lfBClass_16t    *plfBClass_16t;
+typedef lfVBClass_16t   *plfVBClass_16t;
+typedef lfFriendCls_16t *plfFriendCls_16t;
+typedef lfFriendFcn_16t *plfFriendFcn_16t;
+typedef lfMember_16t    *plfMember_16t;
+typedef lfSTMember_16t  *plfSTMember_16t;
+typedef lfVFuncTab_16t  *plfVFuncTab_16t;
+typedef lfMethod_16t    *plfMethod_16t;
+typedef lfOneMethod_16t *plfOneMethod_16t;
+typedef lfNestType_16t  *plfNestType_16t;
+typedef lfNestTypeEx    *plfNestTypeEx;
+typedef lfMemberModify  *plfMemberModify;
+typedef CONSTSYM_16t    *CONSTPTR_16t;
+typedef REGSYM_16t      *REGPTR_16t;
+typedef UDTSYM_16t      *UDTPTR_16t;
+typedef DATASYM32_16t   *DATAPTR32_16t;
+typedef PUBSYM32_16t    *PUBPTR32_16t;
+typedef BPRELSYM32_16t  *BPRELPTR32_16t;
+typedef PROCSYM32_16t   *PROCPTR32_16t;
